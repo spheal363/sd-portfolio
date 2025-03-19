36e2babb013f71e89c43be0f5d6a672750c82c51
spheal363 <115055083+spheal363@users.noreply.github.com>
2025-03-19 02:13:59 +0000

    feature: アイコンを追加

--- components/About.tsx
+++ components/About.tsx
@@ -4,18 +4,30 @@ export default function About() {
     return (
         <div className="flex flex-col md:flex-row items-center justify-center h-screen p-8 bg-white">
             <div className="w-full md:w-1/2 flex flex-col justify-center text-center md:text-left">
-                <h1 className="text-5xl font-bold whitespace-nowrap">
+                <h1 className="text-5xl font-bold whitespace-nowrap mb-6">
                     Hello! I am
                     <br />
                     Sumire Doi
                 </h1>
-                <p className="text-2xl font-light mt-2">
+                <p className="text-2xl font-light mb-6">
                     I’m a master’s student.
                     <br />
                     I’m currently learning Unity!
                 </p>
+                {/* ソーシャルアイコン */}
+                <div className="flex space-x-6 mt-6 justify-center md:justify-start">
+                    <a href="https://github.com/spheal363" target="_blank" rel="noopener noreferrer">
+                        <img src="/icons/github.png" alt="GitHub" className="w-10 h-10" />
+                    </a>
+                    <a href="https://x.com/Spheal_363_poke" target="_blank" rel="noopener noreferrer">
+                        <img src="/icons/x.png" alt="X (Twitter)" className="w-10 h-10" />
+                    </a>
+                    <a href="https://atcoder.jp/users/Spheal_363" target="_blank" rel="noopener noreferrer">
+                        <img src="/icons/atcoder.png" alt="AtCoder" className="w-10 h-10" />
+                    </a>
+                </div>
             </div>
-            <div className="w-full md:w-1/2 flex justify-center mt-8 md:mt-0">
+            <div className="w-full md:w-1/2 flex justify-center mt-12 md:mt-0">
                 <Image
                     src="/images/profile.png"
                     alt="Profile Picture"
