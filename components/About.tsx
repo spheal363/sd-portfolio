7cdc26a536d8c7923509f6954360d2225f889492
spheal363 <115055083+spheal363@users.noreply.github.com>
2025-03-19 00:27:12 +0000

    feature: Aboutコンポーネント

---
+++ components/About.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function About() {
+    return (
+        <div className="flex flex-col md:flex-row items-center justify-center h-screen p-8 bg-white">
+            {/* 左側のテキスト部分 */}
+            <div className="w-full md:w-1/2 flex flex-col justify-center text-center md:text-left">
+                <h1 className="text-5xl font-bold whitespace-nowrap">
+                    Hello! I am
+                    <br />
+                    Sumire Doi
+                </h1>
+                <p className="text-2xl font-light mt-2">
+                    I’m a master’s student.
+                    <br />
+                    I’m currently learning Unity!
+                </p>
+            </div>
+
+            {/* 右側の画像部分 */}
+            <div className="w-full md:w-1/2 flex justify-center mt-8 md:mt-0">
+                <Image
+                    src="/images/profile.png" // public フォルダからの相対パス
+                    alt="Profile Picture"
+                    width={800}
+                    height={800}
+                    className="max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl"
+                />
+            </div>
+        </div>
+    );
+}
